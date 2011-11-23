a53e41e61de1fe8d3885cfbcfbe609448cc56c5d
Stefano Locati <stefano@obliquid.it>
2011-11-23 18:53:04 +0100

    Added querying the datastore

--- src/org/obliquid/goodeatin/GoodEatinServlet.java
+++ src/org/obliquid/goodeatin/GoodEatinServlet.java
@@ -1,8 +1,11 @@
 package org.obliquid.goodeatin;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.logging.Logger;
 
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.*;
@@ -13,14 +16,25 @@ public class GoodEatinServlet extends HttpServlet {
         private static Logger logger = Logger.getLogger(GoodEatinServlet.class.getName());
 
         @Override
+        @SuppressWarnings("unchecked")
         public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException,
                         ServletException {
-
-                // req.setAttribute("restaurants", restList);
-                logger.fine("Restaurants: " + req.getAttribute("restaurants"));
-
-                RequestDispatcher view = req.getRequestDispatcher("WEB-INF/jsp/listView.jsp");
-                view.forward(req, resp);
+                List<Restaurant> restList = null;
+                EntityManager em = EMF.get().createEntityManager();
+                try {
+                        Query query = em.createQuery("SELECT r FROM Restaurant r ORDER BY dateAdded DESC");
+                        //instead of using setFirstResult(), for paging, figure the key of the last
+                        //one and use WHERE, to find keys greater than it.
+                        //query.setFirstResult(0);
+                        query.setMaxResults(3);
+                        restList = query.getResultList();
+                        req.setAttribute("restaurants", restList);
+                        logger.fine("Restaurants: " + req.getAttribute("restaurants"));
+                        RequestDispatcher view = req.getRequestDispatcher("WEB-INF/jsp/listView.jsp");
+                        view.forward(req, resp);
+                } finally {
+                        em.close();
+                }
         }
 
 }
