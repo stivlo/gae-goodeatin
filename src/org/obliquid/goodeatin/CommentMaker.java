7553ae52baee3c0d1dc5c8dad083aa982b988cce
Stefano Locati <stefano@obliquid.it>
2011-11-28 21:42:26 +0800

    Added email sending

--- src/org/obliquid/goodeatin/CommentMaker.java
+++ src/org/obliquid/goodeatin/CommentMaker.java
@@ -1,12 +1,23 @@
 package org.obliquid.goodeatin;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.Properties;
 
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserServiceFactory;
 
 @SuppressWarnings("serial")
 public class CommentMaker extends HttpServlet {
@@ -24,6 +35,8 @@ public class CommentMaker extends HttpServlet {
                                 c.setCommentText(req.getParameter("comment"));
                                 r.getComments().add(c); //this is really bad style
                                 em.persist(r);
+                                User commentSubmitter = UserServiceFactory.getUserService().getCurrentUser();
+                                emailRestaurantSubmitter(r, c, commentSubmitter);
                         }
                         tx.commit();
                 } finally {
@@ -32,6 +45,32 @@ public class CommentMaker extends HttpServlet {
                         }
                 }
                 resp.sendRedirect("/goodEatin");
+        }
+
+        private void emailRestaurantSubmitter(Restaurant r, Comment c, User commentSubmitter) {
+                if (r.getSubmitter() == null) {
+                        return; //ignore restaurants without submitters
+                }
+                Session session = Session.getDefaultInstance(new Properties(), null);
+                StringBuilder body = new StringBuilder("Hello ");
+                body.append(r.getSubmitter().getNickname()).append(",\r\nA new comment was added by ")
+                                .append(commentSubmitter.getNickname()).append(" to \"").append(r.getName())
+                                .append("\":\r\n\r\n").append(c.getCommentText());
+                try {
+                        Message msg = new MimeMessage(session);
+                        //can only set some of the headers
+                        //from must be a registered app administrator or the currently authenticated user
+                        msg.setFrom(new InternetAddress("stivlo@gmail.com", "Good Eatin'"));
+                        msg.addRecipient(Message.RecipientType.TO, new InternetAddress(r.getSubmitter()
+                                        .getEmail(), r.getSubmitter().getNickname()));
+                        msg.setSubject("New comment on your submission");
+                        msg.setText(body.toString());
+                        Transport.send(msg);
+                } catch (UnsupportedEncodingException ex) {
+                        ex.printStackTrace();
+                } catch (MessagingException ex) {
+                        ex.printStackTrace();
+                }
         }
 
 }
